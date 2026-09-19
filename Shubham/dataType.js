d37c3ba7098ade267cb3673e452bb0a20ac02c4f
Shubhammohite0007 <shubhammohite0007@gmail.com>
2026-09-19 18:17:29 +0530

    dataType

--- Shubham/dataType.js
+++ Shubham/dataType.js
@@ -11,11 +11,58 @@ var s="h"
 var s2="10"
 var s3=""
 console.log(s, typeof(s)),console.log(s2, typeof(s2)), console.log(s3, typeof(s3))
+
+var s4="shubham"
+console.log("index of 2 : ",s4[2])
+
+
 //3.boolean 
-//4.undefiend 
-//5.null
+
+//4.undefiend - when we declare va
+var k
+console.log(k , typeof(k))
+
+//5.null intentionally want to keep blank value 
+var h1=null
+console.log(h1, typeof(h1))
 
 //non premetive data type
-//1.array 
-//2.object 
-//3.functiom
+//1.array ; array can store all type of values in sqaure bracket 
+//each value in array has its own index position like string \
+var arr=[2,4.3,'javascript',[3,4,5],{a:12,b:13},true]
+console.log(arr)
+console.log(arr[3])
+//console.log("child value from object",arr[3]['a'])  //form particular object value fetch
+arr.push(100)  //--> add value in  string 
+console.log(arr)
+
+//2.object -store value in key value format in curly braces
+//it only store unique keys 
+//their is no restriction on value 
+var person ={name:"shub",number:12345,age:28}
+console.log(person)
+person.address="dehugaon"  //->add field in object
+console.log(person)
+//delete from object 
+delete person.age
+console.log(person)
+//get user details with key
+console.log(person['number'])
+person['name']="max"
+console.log(person)
+
+
+//3.function
+//block of code which we can reuse n number of time or it reduce duplicasy of code 
+
+function add(n1,n2){
+    //inisilize
+    console.log(add,n1+n2)
+}
+//call of function
+add(10,10)
+console.log(add)
+add(15,10)
+add(15," hello")
+//add(15,hello)
+
---
+++ Shubham/ifConditions.js
@@ -0,0 +1,10 @@
+//  if(condn){
+//     block
+//       }
+
+var n=11
+if(n%2==0){
+    console.log("even",n)
+}else{
+    console.log("odd",n)
+}
